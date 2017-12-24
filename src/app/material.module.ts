0b79ef511e80cac8597377ed9d0ef96089787416
Sandro Huber <sandrohuber89@hotmail.com>
2017-12-24 09:02:02 +0100

    Tested some more material components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatButtonModule } from '@angular/material';
+import {
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
-    imports: [ MatButtonModule ],
-    exports: [ MatButtonModule ]
+    imports: [ MatButtonModule, MatMenuModule, MatIconModule, MatToolbarModule ],
+    exports: [ MatButtonModule, MatMenuModule, MatIconModule, MatToolbarModule ]
 })
 export class MaterialModule {}
