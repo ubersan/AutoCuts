b4756476c03d7adbb44dca536ee3c5234b777590
Sandro Huber <sandrohuber89@hotmail.com>
2018-02-18 06:47:40 +0100

    Moved from powershell to batch script

---
+++ install.bat
@@ -0,0 +1,8 @@
+call npm install
+
+call git clone --recursive https://github.com/libigl/libigl.git
+
+call git clone https://github.com/eigenteam/eigen-git-mirror.git Eigen-3.3.4
+cd "Eigen-3.3.4"
+call git checkout 3.3.4
+cd ".."
